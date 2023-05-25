3cc9c870f4622ee3f661d694487b2966e31d142b
dalija333 <dalija333@gmail.com>
2023-05-25 19:22:34 +0300

    Su CSS

--- pirmas.php
+++ pirmas.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap.min.css">
     <title>Document</title>
 </head>
 <body>
@@ -13,6 +14,16 @@
         $y=10;
     ?>
 
+    <form>
+        <div class="form-group">
+            <label for="formGroupExampleInput">Example label</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+        </div>
+        <div class="form-group">
+            <label for="formGroupExampleInput2">Another label</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+        </div>
+    </form>
     Rezultatas: daugyba <?php echo $x*$y; ?>
     sudėtis <?=$x+$y; ?>
 <a href="pirmas.php?x=8"
